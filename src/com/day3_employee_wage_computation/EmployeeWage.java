f1b827fb20e4dc331c0f7de5ca012d7d16f9ef10
Sourav2707 <smartsourav33@gmail.com>
2022-12-16 16:24:17 +0530

    UC2 calculating employee wage based on attendance

--- src/com/day3_employee_wage_computation/EmployeeWage.java
+++ src/com/day3_employee_wage_computation/EmployeeWage.java
@@ -6,15 +6,20 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
         int fullTime = 1;
-        Random rnd = new Random();
-        int attendCheck = rnd.nextInt(0,2);
-        if(attendCheck == fullTime)
-        {
+        int empHr = 0;
+        int wagePerHr = 20;
+        int empWage = 0;
+        //new operator creates an object from the class by allocating memory for the new object and returning reference to thet memory
+        Random random = new Random();
+        int attendCheck = random.nextInt(0, 2);
+        if (attendCheck == fullTime) {
+            empHr = 8;
             System.out.println("The Employee is present");
+        } else {
+            empHr = 0;
+            System.out.println("The Employee wage is absent");
         }
-        else
-        {
-            System.out.println("The Employee is absent");
-        }
+        empWage = empHr * wagePerHr;
+        System.out.println("The Empployee wage is "+empWage);
     }
 }
