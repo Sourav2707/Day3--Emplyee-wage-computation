98e6776bbb33f7cf05bc1912e810b2b8bbcf8a05
Sourav2707 <smartsourav33@gmail.com>
2022-12-16 16:29:38 +0530

    UC4 adding part time and calculating wage using switch case

--- src/com/day3_employee_wage_computation/EmployeeWage.java
+++ src/com/day3_employee_wage_computation/EmployeeWage.java
@@ -5,28 +5,27 @@ import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
-        int fullTime = 1;
-        int partTime = 2;
+        final int fullTime = 1;
+        final int partTime = 2;
         int empHr = 0;
         int empWage = 0;
         int wagePerHr = 20;
         //new operator creates an object from the class by allocating memory for the new object and returning a reference to thet memory
         Random random = new Random();
         int attendCheck = random.nextInt(0, 3);
-        if (attendCheck == fullTime)
+        switch (attendCheck)
         {
-            empHr = 8;
-            System.out.println("Full time employee");
-        }
-        else if (attendCheck == partTime)
-        {
-            empHr = 4;
-            System.out.println("Part time employee");
-        }
-        else
-        {
-            empHr = 0;
-            System.out.println("The employee is absent");
+            case fullTime:
+                empHr = 8;
+                System.out.println("Full time employee");
+                break;
+            case partTime:
+                System.out.println("Part time employee");
+                break;
+            default:
+                empHr = 0;
+                System.out.println("The Employee is absent");
+                break;
         }
         empWage = empHr * wagePerHr;
         System.out.println("The Employee wage is "+empWage);
