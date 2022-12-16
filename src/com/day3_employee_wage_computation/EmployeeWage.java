b9ce9696f6dcd551bb991d4043f742ffee970d80
Sourav2707 <smartsourav33@gmail.com>
2022-12-16 16:31:10 +0530

    UC5 calculating employee wage for the month

--- src/com/day3_employee_wage_computation/EmployeeWage.java
+++ src/com/day3_employee_wage_computation/EmployeeWage.java
@@ -5,29 +5,37 @@ import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
-        final int fullTime = 1;
-        final int partTime = 2;
+        int fullTime = 1;
+        int partTime = 2;
         int empHr = 0;
         int empWage = 0;
-        int wagePerHr = 20;
-        //new operator creates an object from the class by allocating memory for the new object and returning a reference to thet memory
-        Random random = new Random();
-        int attendCheck = random.nextInt(0, 3);
-        switch (attendCheck)
+        int wagePerHR = 20;
+        int totalWorkingdays = 20;
+        int totalWage = 0;
+        for (int i = 0; i < totalWorkingdays; i++)
         {
-            case fullTime:
+            //new operator creates an object from the class by allocating memory for the new object and returning a reference to thet memory
+            Random random = new Random();
+            int attendCheck = random.nextInt(0, 3);
+            if (attendCheck == fullTime)
+            {
                 empHr = 8;
                 System.out.println("Full time employee");
-                break;
-            case partTime:
+            }
+            else if (attendCheck == partTime)
+            {
+                empHr = 4;
                 System.out.println("Part time employee");
-                break;
-            default:
+            }
+            else
+            {
                 empHr = 0;
-                System.out.println("The Employee is absent");
-                break;
+                System.out.println("The employee is absent");
+            }
+            empWage = empHr * wagePerHR;
+            System.out.println("Employee wage is "+empWage);
+            totalWage = totalWage + empWage;
         }
-        empWage = empHr * wagePerHr;
-        System.out.println("The Employee wage is "+empWage);
+        System.out.println("Total wage for 20 working days is "+totalWage);
     }
 }
