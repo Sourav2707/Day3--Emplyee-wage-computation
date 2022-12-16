420c20e84a77513bcdf1c7b9855ce161763b2d1a
Sourav2707 <smartsourav33@gmail.com>
2022-12-16 16:27:01 +0530

    UC3 adding part time and calculating wages

--- src/com/day3_employee_wage_computation/EmployeeWage.java
+++ src/com/day3_employee_wage_computation/EmployeeWage.java
@@ -6,20 +6,29 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("welcome to employee wage computation program");
         int fullTime = 1;
+        int partTime = 2;
         int empHr = 0;
-        int wagePerHr = 20;
         int empWage = 0;
-        //new operator creates an object from the class by allocating memory for the new object and returning reference to thet memory
+        int wagePerHr = 20;
+        //new operator creates an object from the class by allocating memory for the new object and returning a reference to thet memory
         Random random = new Random();
-        int attendCheck = random.nextInt(0, 2);
-        if (attendCheck == fullTime) {
+        int attendCheck = random.nextInt(0, 3);
+        if (attendCheck == fullTime)
+        {
             empHr = 8;
-            System.out.println("The Employee is present");
-        } else {
+            System.out.println("Full time employee");
+        }
+        else if (attendCheck == partTime)
+        {
+            empHr = 4;
+            System.out.println("Part time employee");
+        }
+        else
+        {
             empHr = 0;
-            System.out.println("The Employee wage is absent");
+            System.out.println("The employee is absent");
         }
         empWage = empHr * wagePerHr;
-        System.out.println("The Empployee wage is "+empWage);
+        System.out.println("The Employee wage is "+empWage);
     }
 }
